1c18cbf74ce9864029db01277f780e2dd2cf8a85
david-asem <ddy.asem@gmail.com>
2021-03-13 11:38:38 +0000

    added dao interface and dao implementation for database communication

---
+++ src/com/david/crm/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.david.crm.dao;
+
+import java.util.List;
+
+import com.david.crm.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+
+}
---
+++ src/com/david/crm/dao/CustomerDAOImplementation.java
@@ -0,0 +1,40 @@
+package com.david.crm.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.david.crm.entity.Customer;
+
+@Repository
+public class CustomerDAOImplementation implements CustomerDAO {
+
+	// need to inject the hibernate session factory
+
+	@Autowired
+
+	private SessionFactory sessionFactory;
+
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+
+		// create a query
+		Query<Customer> newQuery = currentSession.createQuery("from Customer", Customer.class);
+
+		// execute query and get result list of customers
+		List<Customer> customers = newQuery.getResultList();
+
+		// return the results
+		return customers;
+	}
+
+}
